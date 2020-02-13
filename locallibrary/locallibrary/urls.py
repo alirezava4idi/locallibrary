222f6a2f782666b387a26f6bb89ba5223d42930f
alirezava4idi <alireza.vahidi1@gmail.com>
2020-02-13 10:41:07 +0330

    Added the Genre, Language, Book, BookInstance, Author models

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from djnago.views.generic import RedirectView
+from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
